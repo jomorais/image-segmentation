c086f7457c4a5b58c6f0b3decd3e080e3b314baf
Antonio Soares <asoares@vtinova.com.br>
2015-11-24 18:37:36 -0400

    Inicio portabilidade para python do codigo no matlab

--- horprasert_rgb_image.py
+++ horprasert_rgb_image.py
@@ -1,3 +1,21 @@
+from scipy import misc
+import numpy as np
 __author__ = 'jmorais'
 
+N_FRAMES_BG = 64
 
+
+i_soma = 0
+for i in range(N_FRAMES_BG):
+    i_atual = misc.imread('/home/asoares/pos_uea/TrabalhoFinal/IMAGENS DE TESTE/image{}.bmp'.format(i))
+    i_soma += i_atual.astype(float)
+i_media = i_soma/N_FRAMES_BG
+print i_media
+
+s_num_desvio = 0
+for i in range(N_FRAMES_BG):
+    i_atual = misc.imread('/home/asoares/pos_uea/TrabalhoFinal/IMAGENS DE TESTE/image{}.bmp'.format(i))
+    s_num_desvio += np.power(i_atual - i_media, 2)
+desvio_padrao = np.sqrt(s_num_desvio/(N_FRAMES_BG - 1))
+print 'Desvio padrao'
+print desvio_padrao
