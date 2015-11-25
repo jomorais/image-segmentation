de165c1125d53dc5f81f3b760e9a1f7b09960492
Antonio Soares <asoares@vtinova.com.br>
2015-11-25 16:38:38 -0400

    Segmentacao com negativo

--- horprasert_rgb_image.py
+++ horprasert_rgb_image.py
@@ -19,6 +19,10 @@ N_PTS_H_CD = HIST_CD_MAX / GRID
 N_PTS_H_ALFA = HIST_ALFA_MAX / GRID
 F_END_HIST = F_INI_HIST + N_FRAMES_HIST - 1
 
+R = 0
+G = 1
+B = 2
+
 TAXA_DE_ERRO = 0.1
 SAMPLES_OUT = (320 * 240 * N_FRAMES_HIST * TAXA_DE_ERRO) / 100
 
@@ -46,9 +50,9 @@ for i in range(1, N_FRAMES_BG + 1):
     s_num_desvio += np.power(i_atual.astype(np.double) - i_media, 2)
 desvio_padrao = np.sqrt(s_num_desvio / (N_FRAMES_BG - 1))
 
-d_p_r = np.mean(desvio_padrao[..., 0], dtype=np.double)
-d_p_g = np.mean(desvio_padrao[..., 1], dtype=np.double)
-d_p_b = np.mean(desvio_padrao[..., 2], dtype=np.double)
+d_p_r = np.mean(desvio_padrao[..., R], dtype=np.double)
+d_p_g = np.mean(desvio_padrao[..., G], dtype=np.double)
+d_p_b = np.mean(desvio_padrao[..., B], dtype=np.double)
 
 print 'Desvio padrao'
 
@@ -58,7 +62,7 @@ d_p_qb = np.power(d_p_b, 2)
 
 m_q = np.power(i_media, 2)
 
-den = (m_q[..., 0] / d_p_qr) + (m_q[..., 1] / d_p_qg) + (m_q[..., 2] / d_p_qb)
+den = (m_q[..., R] / d_p_qr) + (m_q[..., G] / d_p_qg) + (m_q[..., B] / d_p_qb)
 
 alfa_s = np.zeros((240, 320), dtype=np.double)
 
@@ -68,19 +72,19 @@ for i in range(1, N_FRAMES_BG + 1):
     i_atual = misc.imread('images/image{}.bmp'.format(i)).astype(np.double)
 
     d_n = np.empty_like(i_atual)
-    d_n[:, :, 0] = (i_atual[:, :, 0] * i_media[:, :, 0]) / d_p_qr
-    d_n[:, :, 1] = (i_atual[:, :, 1] * i_media[:, :, 1]) / d_p_qg
-    d_n[:, :, 2] = (i_atual[:, :, 2] * i_media[:, :, 2]) / d_p_qb
+    d_n[:, :, R] = (i_atual[:, :, R] * i_media[:, :, R]) / d_p_qr
+    d_n[:, :, G] = (i_atual[:, :, G] * i_media[:, :, G]) / d_p_qg
+    d_n[:, :, B] = (i_atual[:, :, B] * i_media[:, :, B]) / d_p_qb
 
-    num = d_n[:, :, 0] + d_n[:, :, 1] + d_n[:, :, 2]
+    num = d_n[:, :, R] + d_n[:, :, G] + d_n[:, :, B]
 
     alfa = num / den
 
     alfa_s += np.fix(np.power(alfa - 1, 2))
 
-    CDr = np.power(i_atual[:, :, 0] - (alfa * i_media[:, :, 0]), 2)
-    CDg = np.power(i_atual[:, :, 1] - (alfa * i_media[:, :, 1]), 2)
-    CDb = np.power(i_atual[:, :, 2] - (alfa * i_media[:, :, 2]), 2)
+    CDr = np.power(i_atual[:, :, R] - (alfa * i_media[:, :, R]), 2)
+    CDg = np.power(i_atual[:, :, G] - (alfa * i_media[:, :, G]), 2)
+    CDb = np.power(i_atual[:, :, B] - (alfa * i_media[:, :, B]), 2)
 
     CD = np.sqrt((CDr / d_p_qr) + (CDg / d_p_qg) + (CDb / d_p_qb))
 
@@ -102,29 +106,29 @@ for i in range(201, N_FRAMES_BG + 1):
     frame = misc.imread('images/image{}.bmp'.format(i))
     im_teste = frame.astype(np.double)
 
-    im_teste_r1 = im_teste[:, :, 0]
-    im_teste_g1 = im_teste[:, :, 1]
-    im_teste_b1 = im_teste[:, :, 2]
+    im_teste_r1 = im_teste[:, :, R]
+    im_teste_g1 = im_teste[:, :, G]
+    im_teste_b1 = im_teste[:, :, B]
 
-    im_teste_r = im_teste[:, :, 0]
-    im_teste_g = im_teste[:, :, 1]
-    im_teste_b = im_teste[:, :, 2]
+    im_teste_r = im_teste[:, :, R]
+    im_teste_g = im_teste[:, :, G]
+    im_teste_b = im_teste[:, :, B]
 
     imagem_atual = im_teste
 
     d_n = np.empty_like(imagem_atual)
-    d_n[:, :, 0] = (imagem_atual[:, :, 0] * i_media[:, :, 0]) / d_p_qr
-    d_n[:, :, 1] = (imagem_atual[:, :, 1] * i_media[:, :, 1]) / d_p_qg
-    d_n[:, :, 2] = (imagem_atual[:, :, 2] * i_media[:, :, 2]) / d_p_qb
+    d_n[:, :, R] = (imagem_atual[:, :, R] * i_media[:, :, R]) / d_p_qr
+    d_n[:, :, G] = (imagem_atual[:, :, G] * i_media[:, :, G]) / d_p_qg
+    d_n[:, :, B] = (imagem_atual[:, :, B] * i_media[:, :, B]) / d_p_qb
 
-    num = d_n[:, :, 0] + d_n[:, :, 1] + d_n[:, :, 2]
+    num = d_n[:, :, R] + d_n[:, :, G] + d_n[:, :, B]
     alfa = num / den
 
     alfa_n = (alfa - 1) / alfa_rms
 
-    CDr = np.power((imagem_atual[:, :, 0] - (alfa * i_media[:, :, 0])), 2)
-    CDg = np.power((imagem_atual[:, :, 1] - (alfa * i_media[:, :, 1])), 2)
-    CDb = np.power((imagem_atual[:, :, 2] - (alfa * i_media[:, :, 2])), 2)
+    CDr = np.power((imagem_atual[:, :, R] - (alfa * i_media[:, :, R])), 2)
+    CDg = np.power((imagem_atual[:, :, G] - (alfa * i_media[:, :, G])), 2)
+    CDb = np.power((imagem_atual[:, :, B] - (alfa * i_media[:, :, B])), 2)
     CD = np.sqrt((CDr / d_p_qr) + (CDg / d_p_qg) + (CDb / d_p_qb))
     CD_norm = CD / CD_rms
 
@@ -136,27 +140,27 @@ for i in range(201, N_FRAMES_BG + 1):
     alfa_lim = np.where(alfa < G_ALFA_MIN)
     alfa_lim1 = np.where(alfa > G_ALFA_MAX)
 
-    imfinal = np.zeros((RES_HOR, RES_VERT, 3), dtype=np.double)
-    imfinal_r = np.zeros((RES_HOR, RES_VERT), dtype=np.double)
-    imfinal_g = np.zeros((RES_HOR, RES_VERT), dtype=np.double)
-    imfinal_b = np.zeros((RES_HOR, RES_VERT), dtype=np.double)
+    imfinal = np.zeros((RES_HOR, RES_VERT, 3))
 
-    im_teste_r = im_teste_r.astype(np.uint8)
-    im_teste_g = im_teste_g.astype(np.uint8)
-    im_teste_b = im_teste_b.astype(np.uint8)
+    imfinal_r = np.zeros((RES_HOR, RES_VERT))
+    imfinal_g = np.zeros((RES_HOR, RES_VERT))
+    imfinal_b = np.zeros((RES_HOR, RES_VERT))
+
+    im_teste_r = im_teste_r.astype(np.int)
+    im_teste_g = im_teste_g.astype(np.int)
+    im_teste_b = im_teste_b.astype(np.int)
+    print im_teste_r[objeto1]
 
     imfinal_r[objeto1] = im_teste_r[objeto1]
     imfinal_g[objeto1] = im_teste_g[objeto1]
     imfinal_b[objeto1] = im_teste_b[objeto1]
 
-    imfinal[:, :, 0] = imfinal_r
-    imfinal[:, :, 1] = imfinal_g
-    imfinal[:, :, 2] = imfinal_b
-
-    imtestefinal = [frame, imfinal]
+    imfinal[:, :, R] = imfinal_r
+    imfinal[:, :, G] = imfinal_g
+    imfinal[:, :, B] = imfinal_b
 
     plt.subplot(121)
     plt.imshow(frame)
     plt.subplot(122)
-    plt.imshow(imfinal)
+    plt.imshow(np.negative(imfinal))
     plt.show()
