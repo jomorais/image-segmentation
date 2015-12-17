9d50614f3706e305caf9c338cc20702cdd97da8d
nilson <nilsonmont.o@gmail.com>
2015-12-17 17:48:17 -0400

    Inclusao de arquivo de configuracao da camera do raspberry..

---
+++ config-RPI-cam.py
@@ -0,0 +1,17 @@
+__author__ = 'nilson'
+
+import subprocess, time
+
+print 'intanciating driver..'
+subprocess.call(["modprobe", "bcm2835-v4l2"])
+
+time.sleep(1)
+
+print 'switching auto-exposure off..'
+subprocess.call(["v4l2-ctl", "-c", "auto-exposure=1"])
+print 'switching white_balance to manual..'
+subprocess.call(["v4l2-ctl", "-c", "white_balance_auto_preset=0"])
+print 'disabling power_line_frequency detection..'
+subprocess.call(["v4l2-ctl", "-c", "power_line_frequency=0"])
+print 'decreasing saturation set point (ideally it should be fixed but..)'
+subprocess.call(["v4l2-ctl", "-c", "saturation=-30"])
